869c78038ae1477334ef34f6727af2c1cda4a692
Caleb Finney <calebfinney45@gmail.com>
2026-08-03 21:47:51 +0300

    Define Exercise, Workout, and WorkoutExercise models

--- server/models.py
+++ server/models.py
@@ -2,3 +2,34 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 
 db = SQLAlchemy()
+
+VALID_CATEGORIES = ["cardio", "strength", "flexibility", "balance", "plyometric"]
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False, default=False)
+
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
+
+
+class WorkoutExercise(db.Model):
+    __tablename__ = "workout_exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey("workouts.id"), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey("exercises.id"), nullable=False)
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
